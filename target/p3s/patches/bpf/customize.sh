117f8e2f3beee0e150f51cac014d2fb483d7731d
Your Name <you@example.com>
2026-03-20 21:07:51 +0100

    fixed spen

--- target/p3s/patches/bpf/customize.sh
+++ target/p3s/patches/bpf/customize.sh
@@ -1,4 +1,3 @@
 cp -fa "$SRC_DIR/target/p3s/patches/bpf/proton/img/boot.img" "$WORK_DIR/kernel/boot.img"
 cp -fa "$SRC_DIR/target/p3s/patches/bpf/proton/img/vendor_boot.img" "$WORK_DIR/kernel/vendor_boot.img"
-cp -fa "$SRC_DIR/target/p3s/patches/bpf/proton/img/dtbo.img" "$WORK_DIR/kernel/dtbo.img"
 
